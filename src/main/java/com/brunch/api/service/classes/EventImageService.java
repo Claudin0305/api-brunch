8a237e0ae2fe35de8b3ca20050205afa66a762be
Claudin Saintil <claudinsaintil@gmail.com>
2023-07-13 22:35:28 -0400

    refactor upload

--- src/main/java/com/brunch/api/service/classes/EventImageService.java
+++ src/main/java/com/brunch/api/service/classes/EventImageService.java
@@ -15,10 +15,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.PosixFilePermission;
-import java.nio.file.attribute.PosixFilePermissions;
 import java.util.List;
-import java.util.Set;
 import java.util.UUID;
 
 @Service
@@ -33,9 +30,7 @@ public class EventImageService {
                 .toAbsolutePath().normalize();
 
         try {
-            String permissions = "rwxr-x---";
-            Set<PosixFilePermission> posixPermissions = PosixFilePermissions.fromString(permissions);
-            Files.createDirectories(this.fileStorageLocation, PosixFilePermissions.asFileAttribute(posixPermissions));
+            Files.createDirectories(this.fileStorageLocation);
         } catch (Exception ex) {
             throw new RuntimeException(
                     "Could not create the directory where the uploaded files will be stored.", ex);
