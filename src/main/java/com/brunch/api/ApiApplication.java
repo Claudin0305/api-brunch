d4114d6d5b7b4bdfd4f41e6a3dd4ada862721e36
Claudin Saintil <claudinsaintil@gmail.com>
2023-07-14 16:45:15 -0400

    refactor security added

--- src/main/java/com/brunch/api/ApiApplication.java
+++ src/main/java/com/brunch/api/ApiApplication.java
@@ -1,20 +1,11 @@
 package com.brunch.api;
 
-import com.brunch.api.entity.ApplicationUser;
-import com.brunch.api.entity.Role;
-import com.brunch.api.repository.RoleRepository;
-import com.brunch.api.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.HashSet;
-import java.util.Set;
-
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableJpaAuditing
 public class ApiApplication {
 
