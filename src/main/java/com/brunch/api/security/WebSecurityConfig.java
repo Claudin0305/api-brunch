28cf2e03b971e6ccd5415daeda5f1e93a44c1572
Claudin Saintil <claudinsaintil@gmail.com>
2023-07-22 21:06:17 -0400

    refactoring

--- src/main/java/com/brunch/api/security/WebSecurityConfig.java
+++ src/main/java/com/brunch/api/security/WebSecurityConfig.java
@@ -57,7 +57,7 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()).cors().disable()
+        http.csrf(csrf -> csrf.disable())
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
